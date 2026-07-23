035c8870de08030c19404d67acd7bf45dc1a1299
taiiok <bolobolbfmv@gmail.com>
2026-07-23 19:34:39 +0300

    chore(release): bump version to 1.0.7 and update versionCode to 8
    Updated version numbers in app.config.ts, app.json, and package.json to reflect the new release version 1.0.7 and incremented versionCode to 8 for Android builds.

--- app.config.ts
+++ app.config.ts
@@ -14,7 +14,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: isTvBuild ? 'Sleek TV' : 'Sleek',
   slug: 'sleek',
-  version: '1.0.6',
+  version: '1.0.7',
   scheme: 'sleek',
   icon: './assets/images/icon.png',
   userInterfaceStyle: 'automatic',
@@ -43,7 +43,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     },
     predictiveBackGestureEnabled: false,
     package: isTvBuild ? 'ru.taiiok.aniverse.tv' : 'ru.taiiok.aniverse.app',
-    versionCode: 7,
+    versionCode: 8,
     intentFilters: [
       {
         action: 'VIEW',
